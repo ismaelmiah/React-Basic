0271e2b3667febf5920e140fc7627bea997f52ad
Ismail Hossen <ismail27.dec@gmail.com>
2021-05-08 00:10:19 +0600

    tailwindcss configured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import chat from './chat.svg';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+     <div className="chat-notification">
+      <div className="chat-notification-logo-wrapper">
+        <img className="chat-notification-logo" src={chat} alt="ChitChat Logo" />
+      </div>
+      <div className="chat-notification-content">
+        <h4 className="chat-notification-title">ChitChat</h4>
+        <p className="chat-notification-message">You have a new message!</p>
+      </div>
+    </div>
+    
     </div>
   );
 }
